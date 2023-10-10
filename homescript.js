20b837f9d6b130401840905a6a5684c7998a4a59
Kaleb Crans <69497802+kcrans@users.noreply.github.com>
2023-10-10 15:09:01 -0700

    Update homescript.js

--- homescript.js
+++ homescript.js
@@ -1,13 +1,21 @@
 document.getElementById('CopyButton').addEventListener('click', function(event) {
-    event.stopPropagation();  // Prevent triggering the default <summary> behavior
-    
-    var textToCopy = document.getElementById('pgp_key').textContent;
-    var textArea = document.createElement('textarea');
-    textArea.value = textToCopy;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
+  event.stopPropagation();  // Prevent triggering the default <summary> behavior
+  var textToCopy = document.getElementById('pgp_key').textContent;
+  var clippy = document.getElementById('clipboard_svg');
+  var checky = document.getElementById('checkmark_svg');
+  var texty = document.getElementById('button_text');
+  
+  navigator.clipboard.writeText(textToCopy).then(() => {
+    clippy.style.display = 'none';
+    checky.style.display = 'block';
+    texty.textContent = "Copied!";
+  }).catch(err => {
+    console.error('Unable to copy text: ', err);
+  });
 
-    alert('Text copied to clipboard!');
+  setTimeout(() => {
+    clippy.style.display = 'block';
+    checky.style.display = 'none';
+    texty.textContent = "Copy key";
+  }, 3000)
 });
